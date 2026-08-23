ff4588105dae3cadcaeaeb1488cd3014571befab
Sunil Prajapati <2023cssunil14308@poornima.edu.in>
2026-08-23 17:06:57 +0530

    added frontend deployed url

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://bpb-opal.vercel.app/',
     credentials: true,
 }))
 
